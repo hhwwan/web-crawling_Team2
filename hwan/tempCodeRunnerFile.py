25e64609571fbada82e2f6b9328708e38eff1107
김동환 <120583341+hhwwan@users.noreply.github.com>
2024-12-05 13:45:20 +0900

    Delete hwan directory

--- hwan/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-print(f"Calories: {calories} kcal")
-                # print(f"Protein: {protein} g")
-                # print(f"Fat: {fat} g")
-                # print(f"Carbohydrates: {carbohydrates} g")
